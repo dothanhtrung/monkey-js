8778bc02654328be283a91eb40a8a0ee9ff7f7cf
Trung Do <onlyesterday.16@gmail.com>
2020-04-10 09:39:09 +0000

    youtube-nocookie: Update to replace data-embed-src

--- youtube-nocookie.user.js
+++ youtube-nocookie.user.js
@@ -1,20 +1,27 @@
 // ==UserScript==
 // @name     youtube-nocookie
-// @version  1
+// @description  Replace frame youtube embed by youtube-nocookie
+// @version  1.1
 // @grant    none
 // @match    *://*/*
 // ==/UserScript==
 
-// Copyright 2018 Trung Do
+// Copyright 2020 Trung Do
 
 var iframes = document.getElementsByTagName("iframe");
 
 for (var i = 0; i < iframes.length; i++) {
 	var iframe = iframes[i];
+  
 	var link = iframe.src;
-
-	if (link.indexOf("youtube.com/embed") !== -1){
-		var new_link = link.replace("youtube.com/embed","youtube-nocookie.com/embed");
-		iframe.src = new_link;
-	}
+  if (link) {
+    var new_link = link.replace("youtube.com/embed","youtube-nocookie.com/embed");
+    iframe.src = new_link;
+  }
+  
+  link = iframe.getAttribute("data-embed-src");
+  if (link) {
+    var new_link = link.replace("youtube.com/embed","youtube-nocookie.com/embed");
+    iframe.setAttribute("data-embed-src", new_link);
+  }
 }
